3c212dfcb32e7945a8ce7598213fd66e2f2f306b
Michael Färber <01mf02@gmail.com>
2013-02-08 23:37:30 +0100

    Cleanup.

--- gnf.ml
+++ gnf.ml
@@ -34,18 +34,21 @@ let string_of_variable_rule = function
 let string_of_variable_rules (r : variable_rules) =
   String.concat " | " (map string_of_variable_rule r);;
 
+(* production_rules -> string *)
+let string_of_production_rules (r : production_rules) =
+  let rec aux i rules = match rules with
+  | [] -> []
+  | head::tail ->
+    (Format.sprintf "X%d -> %s" i (string_of_variable_rules head))::
+	(aux (i+1) tail) in
+  String.concat "\n" (aux 0 r);;
+
 (* base -> string *)
 let string_of_base (bs : base) = String.concat ","
   (map (function (j,i,rest) ->
 		"(" ^ (string_of_variable j) ^ "," ^
 		(string_of_variable i) ^ (string_of_variables rest) ^ ")") bs);;
-
-(* production_rules -> unit *)
-let print_production_rules (r : production_rules) =
-  for i = 0 to (length r)-1 do
-    Format.printf "X%d -> %s\n" i (string_of_variable_rules (nth r i));
-  done;;
-
+    
 
 (************************************************
  *************** Auxiliary functions ************
@@ -53,22 +56,33 @@ let print_production_rules (r : production_rules) =
 
 (* return first n elements of a list *)
 (* int -> 'a list -> 'a list *)
-let rec first_n (n : int) = function
+let rec first_n n = function
   | [] -> []
   | head::tail -> if n > 0 then head::(first_n (n-1) tail) else [];;
 
-let rec range (i : int) (j : int) = if i > j then [] else i::(range (i+1) j)
+(* take two lists, shorten them to length of smaller list and combine them *)
+(* 'a list -> 'a list -> ('a * 'a) list *)
+let equalize_combine l1 l2 =
+  let min_len = min (length l1) (length l2) in
+  let l1_cut = first_n min_len l1 in
+  let l2_cut = first_n min_len l2 in
+  (combine l1_cut l2_cut);;
+
+(* construct a list of integers [i; i+1; ... ;j] *)
+(* int -> int -> int list *)
+let rec range i j = if i > j then [] else i::(range (i+1) j);;
+
 
 
 (************************************************
  ***************** Norm functions ***************
  ************************************************)
 
-(* calculate the norm of the i-th variable of a grammar *)
-(* int -> production_rules -> int *)
-let rec norm (i : int) (prods : production_rules) =
-  let vars = snd (hd (nth prods i)) in
-  fold_left (fun prev_sum var -> prev_sum + (norm var prods)) 1 vars;;
+(* calculate the norm of a variable of a grammar *)
+(* variable -> production_rules -> int *)
+let rec norm (v : variable) (prods : production_rules) =
+  let vars = snd (hd (nth prods v)) in
+  fold_left (fun prev_sum curr_var -> prev_sum + (norm curr_var prods)) 1 vars;;
 
 exception Negative_norm_reduce
 
@@ -111,16 +125,13 @@ let base_equals (bs : base) (a : variables) (b : variables) =
   let rec base_eq (g : variable -> variable * variable list) =
     let to_list = function (i, rest) -> i::rest in
     let gstar = fold_left (fun prev curr -> prev @ to_list (g curr)) [] in
-	let gsa = gstar a and gsb = gstar b in
-	if gsa = gsb then
+	let ga = gstar a and gb = gstar b in
+	if ga = gb then
 	  true
 	else
-	  let min_len = min (length gsa) (length gsb) in
-	  let gsa_cut = first_n min_len gsa in
-	  let gsb_cut = first_n min_len gsb in
-	  let gsab_cut = (combine gsa_cut gsb_cut) in
+	  let gab = equalize_combine ga gb in
 	  try
-	    let mismatch = find (function (x,y) -> x <> y) gsab_cut in
+	    let mismatch = find (function (x,y) -> x <> y) gab in
 	    match mismatch with (ma, mb) ->
 	      let (i, j) = (min ma mb, max ma mb) in
 		  let elem = find (function (x,y,_) -> x = j && y = i) bs in
@@ -145,7 +156,8 @@ let rec refine_base (bs : base) (prods : production_rules) =
 	  for_all (fun q -> exists (fun p -> check p q) range_i) range_j &&
 	  for_all (fun p -> exists (fun q -> check p q) range_j) range_i in
 
-  print_endline (string_of_base bs);
+  print_endline ("Refine base: " ^ (string_of_base bs));
+
   let refined = filter valid_pair bs in
   if bs = refined then
     bs
@@ -160,6 +172,8 @@ let rec refine_base (bs : base) (prods : production_rules) =
 let _ =
   (* let prods = [[('a', []); ('a', [1])];
                [('b', [0])]] in *)
+  (* let prods = [[('a', []); ('b', [1])];
+               [('a', []); ('b', [0])]] in *)
   let prods = [[('a', []); ('a', [0])];
                [('a', []); ('a', [1]); ('a', [2])];
 			   [('a', [])]] in
@@ -169,7 +183,7 @@ let _ =
   let r_base = refine_base i_base prods in
 
   print_endline "Production rules:";
-  print_production_rules prods;
+  print_endline (string_of_production_rules prods);
 
   for i = 0 to (length prods)-1 do
     Format.printf "Variable %d has norm: %d\n" i (norm i prods);
