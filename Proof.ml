d2ad867f24a22a70364fff26a439cf4d942216bf
Michael Färber <01mf02@gmail.com>
2013-02-15 10:33:20 +0100

    Fixed bug, renamed some things.

--- Proof.ml
+++ Proof.ml
@@ -2,10 +2,10 @@ open Format;;
 open Gnf;;
 open List;;
 
-type expr = Product of variables | Sum of variable_rules;;
-type equiv = expr * expr;;
+type expression = Product of variables | Sum of variable_rules;;
+type equivalence = expression * expression;;
 
-type sequent = equiv * rule and rule =
+type sequent = equivalence * rule and rule =
     Refl
   | Gr
   | Unsupported
@@ -14,15 +14,15 @@ type sequent = equiv * rule and rule =
   | Times of sequent * sequent
   | Trans of sequent * sequent;;
 
-let string_of_expr = function
+let string_of_expression = function
   | Product p -> string_of_variables p
   | Sum s -> string_of_variable_rules s;;
 
-let string_of_equiv = function (a, b) ->
-  (string_of_expr a) ^ " = " ^ (string_of_expr b);;
+let string_of_equivalence = function (a, b) ->
+  (string_of_expression a) ^ " = " ^ (string_of_expression b);;
 
 let rec string_of_sequent = function (e, r) ->
-  (string_of_equiv e) ^ " -> " ^ (string_of_rule r) and
+  (string_of_equivalence e) ^ " -> " ^ (string_of_rule r) and
 string_of_sequents seqs = String.concat ", " (map string_of_sequent seqs) and
 string_of_rule = function
   | Refl -> "Refl"
@@ -38,7 +38,7 @@ exception Empty_product;;
 exception Empty_sum;;
 exception Proof_impossible;;
 
-let prove_eq e prods base =
+let prove_equivalence eq prods base =
   let partition v1 v2 =
     let norm_vars vars = fold_left (fun s v -> s + (norm v prods)) 0 in
 
@@ -58,17 +58,17 @@ let prove_eq e prods base =
       else raise Proof_impossible in
     partition' [] v1 [] v2 in
 
-  let rec prove_eq' = function (a, b) ->
+  let rec prove_eq e = match e with (a, b) ->
     if a = b then
       (a, b), Refl
     else match a with
       | Product [] -> raise Empty_product
       | Product (pah::[]) -> let gr = Sum(nth prods pah) in
-          e, Trans(((a, gr), Gr), prove_eq' (gr, b))
+          e, Trans(((a, gr), Gr), prove_eq (gr, b))
       | Product (pah::pat) ->
           begin match b with
             | Product [] -> raise Empty_product
-            | Product (pbh::[]) -> e, Sym(prove_eq' (b, a))
+            | Product (pbh::[]) -> e, Sym(prove_eq (b, a))
             | Product (pbh::pbt) -> e, Unsupported
             | Sum [] -> raise Empty_sum
             | _ -> e, Unsupported
@@ -77,23 +77,23 @@ let prove_eq e prods base =
       | Sum ((sahc, sahv)::[]) ->
           begin match b with
             | Product [] -> raise Empty_product
-            | Product (pbh::[]) -> e, Sym(prove_eq' (b, a))
+            | Product (pbh::[]) -> e, Sym(prove_eq (b, a))
             | Product (pbh::pat) -> raise Proof_impossible
             | Sum [] -> raise Empty_sum
             | Sum ((sbhc, sbhv)::[]) ->
                 if sahc = sbhc then
                   let sum_of_char c = Sum([(c, [])]) in
                   e, Times(((sum_of_char sahc, sum_of_char sbhc), Refl),
-                           prove_eq' (Product(sahv), Product(sbhv)))
+                           prove_eq (Product(sahv), Product(sbhv)))
                 else
                   raise Proof_impossible
             | Sum (sbh::sbt) -> raise Proof_impossible
           end
       | Sum (sah::sat) -> e, Unsupported in
 
-  prove_eq' e;;
+  prove_eq eq;;
 
-let prove_var_eq a b = prove_eq (Product [a], Product [b]);;
+let prove_var_eq a b = prove_equivalence (Product [a], Product [b]);;
 
 let _ =
   let p0 = [[('a', [])]; [('a', [])]] in
