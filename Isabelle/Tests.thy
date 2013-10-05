8c17689ce9d1766d05c350641a21788686304485
Michael Färber <01mf02@gmail.com>
2013-10-05 02:00:15 +0200

    New test!

--- Isabelle/Tests.thy
+++ Isabelle/Tests.thy
@@ -10,5 +10,6 @@ value "gram_valid test_gr"
 value "gram_max_vars test_gr"
 value "norms_of_grammar test_gr"
 value "word_in_variables test_gr ''a'' [0]"
+value "minimal_word_of_variables test_gr [1]"
 
 end
