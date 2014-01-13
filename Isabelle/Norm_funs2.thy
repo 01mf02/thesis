1a9f79fe7ea73526f421925c502704ab0fa8e019
Michael Färber <01mf02@gmail.com>
2014-01-13 19:21:14 +0100

    Make new version of norm functions/proofs to try new idea.

---
+++ Isabelle/Norm_funs2.thy
@@ -0,0 +1,128 @@
+header {* Norm functions *}
+
+theory Norm_funs2 imports
+  Norm_defs
+  Partition_iterate
+begin
+
+
+subsection {* Types *}
+
+type_synonym ('t, 'v) t_rule_norm   = "nat \<times> ('t, 'v) t_rule"
+type_synonym ('t, 'v) t_rules_norms = "('t, 'v) t_rule_norm list"
+type_synonym ('t, 'v) v_rule_norm   = "('v \<times> ('t, 'v) t_rule_norm)"
+type_synonym ('t, 'v) grammar_norms = "('t, 'v) v_rule_norm list"
+
+
+subsection {* Functions *}
+
+definition norm_sum :: "('t, 'v) grammar_norms \<Rightarrow> 'v list \<Rightarrow> nat" where
+  "norm_sum norms vars \<equiv> listsum (map (fst \<circ> lookup norms) vars)"
+
+definition t_rule_has_norm :: "('t, 'v) grammar_norms \<Rightarrow> ('t, 'v) t_rule \<Rightarrow> bool" where
+  "t_rule_has_norm norms \<equiv> \<lambda>(t, vs). set vs \<subseteq> keys norms"
+
+definition t_rules_have_norm :: "('t, 'v) grammar_norms \<Rightarrow> ('t, 'v) t_rules \<Rightarrow> bool" where
+  "t_rules_have_norm norms rules \<equiv> \<exists>r \<in> set rules. t_rule_has_norm norms r"
+
+definition norm_of_t_rule ::
+  "('t, 'v) grammar_norms \<Rightarrow> ('t, 'v) t_rule \<Rightarrow> ('t, 'v) t_rule_norm" where
+  "norm_of_t_rule norms \<equiv> \<lambda>(t, vs). (Suc (norm_sum norms vs), (t, vs))"
+
+definition norms_of_t_rules ::
+  "('t, 'v) grammar_norms \<Rightarrow> ('t, 'v) t_rules \<Rightarrow> ('t, 'v) t_rules_norms" where
+  "norms_of_t_rules norms rules \<equiv>
+     map (norm_of_t_rule norms) (filter (t_rule_has_norm norms) rules)"
+
+definition min_norm_of_t_rules :: "('t::linorder, 'v::linorder) grammar_norms \<Rightarrow>
+  ('t, 'v) t_rules \<Rightarrow> ('t, 'v) t_rule_norm" where
+  "min_norm_of_t_rules norms rules \<equiv> Min (set (norms_of_t_rules norms rules))"
+
+definition v_rule_has_norm ::
+  "('t::wellorder, 'v::wellorder) grammar_norms \<Rightarrow> ('t, 'v) v_rule \<Rightarrow> bool" where
+  "v_rule_has_norm = (\<lambda>norms (v, rules). t_rules_have_norm norms rules)"
+
+(* TODO: Use map_ran here? *)
+definition mnotr_map ::
+  "('t::wellorder, 'v::wellorder) grammar_norms \<Rightarrow> ('t, 'v) grammar \<Rightarrow> ('t, 'v) grammar_norms" where
+  "mnotr_map norms = map (\<lambda>(v, rules). (v, min_norm_of_t_rules norms rules))"
+
+definition v_rules_of_norms ::
+  "('t::wellorder, 'v::wellorder) grammar_norms \<Rightarrow> ('t, 'v) grammar \<Rightarrow> ('t, 'v) grammar" where
+  "v_rules_of_norms norms gr = map (\<lambda>(v, n, t, vs). (v, lookup gr v)) norms"
+
+definition add_norms ::
+  "('t::wellorder, 'v::wellorder) grammar_norms \<Rightarrow> ('t, 'v) grammar \<Rightarrow> ('t, 'v) grammar_norms" where
+  "add_norms norms yes = norms @ (mnotr_map norms yes)"
+
+definition iterate_norms ::
+  "('t :: wellorder, 'v :: wellorder) grammar \<Rightarrow> (('t, 'v) grammar_norms \<times> ('t, 'v) grammar)" where
+  "iterate_norms gr = partition_iterate v_rule_has_norm add_norms [] gr"
+
+definition itno_invariant where
+  "itno_invariant gr norms rest \<equiv> set rest \<subseteq> set gr \<and> keys gr = keys norms \<union> keys rest"
+
+definition itno_invariant_sd where
+  "itno_invariant_sd gr norms rest \<equiv> is_alist norms \<and> is_alist rest \<and> keys rest \<inter> keys norms = {}"
+
+definition nog_invariant where
+  "nog_invariant norms v rules \<equiv>
+     t_rules_have_norm norms rules \<and> (v, min_norm_of_t_rules norms rules) \<in> set norms"
+
+definition nog_invariant_n_t_vs where
+  "nog_invariant_n_t_vs norms rules n t vs \<equiv>
+     t_rules_have_norm norms rules \<and> (n, t, vs) = min_norm_of_t_rules norms rules"
+
+definition gram_normed_fun :: "('t :: wellorder, 'v :: wellorder) grammar \<Rightarrow> bool" where
+  "gram_normed_fun gr \<equiv> snd (iterate_norms gr) = []"
+
+definition gram_nsd_fun :: "('t :: wellorder, 'v :: wellorder) grammar \<Rightarrow> bool" where
+  "gram_nsd_fun gr \<equiv> gram_sd gr \<and> gram_normed_fun gr"
+
+definition refine_norms ::
+  "('t::wellorder, 'v::wellorder) grammar_norms \<Rightarrow> ('t, 'v) grammar \<Rightarrow> ('t, 'v) grammar_norms" where
+  "refine_norms norms gr = mnotr_map norms (v_rules_of_norms norms gr)"
+
+definition rn_invariant where
+  "rn_invariant norms gr \<equiv>
+     (\<forall>(v, norm) \<in> set norms. norm \<in> set (norms_of_t_rules norms (lookup gr v))) \<and>
+     is_alist norms \<and> keys norms \<subseteq> keys gr"
+
+function minimise_norms ::
+  "('t::wellorder, 'v::wellorder) grammar_norms \<Rightarrow> ('t, 'v) grammar \<Rightarrow> ('t, 'v) grammar_norms" where
+  "minimise_norms norms gr = (
+     if is_alist gr \<and> rn_invariant norms gr \<and> refine_norms norms gr \<noteq> norms then
+       minimise_norms (refine_norms norms gr) gr
+     else norms)"
+by auto
+
+definition norms_of_grammar ::
+  "('t :: wellorder, 'v :: wellorder) grammar \<Rightarrow> ('t, 'v) grammar_norms" where
+  "norms_of_grammar gr \<equiv> minimise_norms (fst (iterate_norms gr)) gr"
+
+definition norm_fun :: "('t :: wellorder, 'v :: wellorder) grammar \<Rightarrow> 'v list \<Rightarrow> nat" where
+  "norm_fun gr vars \<equiv> norm_sum (norms_of_grammar gr) vars"
+
+function min_word_of_variables ::
+  "('t :: wellorder, 'v :: wellorder) grammar \<Rightarrow> 'v list \<Rightarrow> 't list" where
+  "min_word_of_variables gr vars = (
+     if gram_nsd_fun gr \<and> set vars \<subseteq> keys gr then
+       concat (map (\<lambda>(n, t, vs). t # (min_word_of_variables gr vs))
+                   (map (lookup (norms_of_grammar gr)) vars))
+     else [])"
+by auto
+
+
+(*****************************************************************************
+  Norm reduction
+ *****************************************************************************)
+
+fun norm_reduce :: "('t :: wellorder, 'v :: wellorder) grammar_norms \<Rightarrow> 'v list \<Rightarrow> nat \<Rightarrow> 'v list" where
+  "norm_reduce norms v 0 = v"
+| "norm_reduce norms [] (Suc p) = []"
+| "norm_reduce norms (vh#vt) (Suc p) = (
+     let (n, _, v) = lookup norms vh in
+     if Suc p < n then (norm_reduce norms v p) @ vt
+     else norm_reduce norms vt (Suc p - n))"
+
+end
