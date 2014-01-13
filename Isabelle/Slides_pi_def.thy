576e600ed7885cf6109559df2a183001c7fc2227
Michael Färber <01mf02@gmail.com>
2014-01-13 09:12:17 +0100

    Polished presentation.

---
+++ Isabelle/Slides_pi_def.thy
@@ -0,0 +1,10 @@
+(*<*)theory Slides_pi_def imports
+  Partition_iterate
+  "~~/src/HOL/Library/LaTeXsugar"
+begin(*>*)
+
+text {*
+@{thm [display] partition_iterate.simps}
+*}
+
+(*<*)end(*>*)
--- Isabelle/Slides_proofs.thy
+++ Isabelle/Slides_proofs.thy
@@ -5,9 +5,17 @@ begin(*>*)
 
 text {*
 \lyxframe{Central theorem}
+
+\begin{block}{Norm function}
+@{thm norm_fun_def}
+\end{block}
+
+\begin{block}{Correctness theorem}
 \begin{center}
 @{thm[mode=Rule] nf_calcs_nd}
 \end{center}
+\end{block}
+
 \lyxframeend{}
 *}
 
