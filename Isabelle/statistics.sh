fbd89d7200b02f800d22cd806abde597baf80ba4
Michael Färber <01mf02@gmail.com>
2014-01-30 18:54:13 +0100

    Embed statistics into report.

--- Isabelle/statistics.sh
+++ Isabelle/statistics.sh
@@ -1,5 +1,4 @@
 #!/bin/sh
-printf '\\begin{itemize}\n'
 
 USAGE=$(($(cat *.thy | wc -c) / 1024))
 LINES=`cat *.thy | grep -c "[^ ]"`
@@ -11,7 +10,3 @@ LEMS=`grep -e '^lemma' -e '^theorem' *.thy | wc -l`
 printf '\\item Number of definitions: %s\n' "$DEFS"
 printf '\\item Number of functions: %s\n' "$FUNS"
 printf '\\item Number of lemmata: %s\n' "$LEMS"
-
-printf '\\onslide<2->\\item Amount of hair lost fighting with Isabelle: ...\n'
-
-printf '\\end{itemize}\n'
